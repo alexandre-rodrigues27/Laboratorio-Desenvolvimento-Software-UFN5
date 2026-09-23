c68fa2e7f4e6da927fc2ffeebf9c25fc6ec81d74
Alexandre Kikuchi Rodrigues <rodriguesxandy82@gmail.com>
2026-09-23 12:25:29 -0300

    Update escola.sql

--- Aula 23-09 (BD + Java)/escola.sql
+++ Aula 23-09 (BD + Java)/escola.sql
@@ -1,1 +1,52 @@
+create database escola;
 
+use escola;
+
+CREATE TABLE aluno(
+id int auto_increment PRIMARY KEY,
+nome varchar(50) NOT NULL,
+idade int NOT NULL,
+curso varchar(50) NOT NULL
+);
+
+INSERT INTO aluno(nome, idade,curso)
+VALUES
+("João", 20, "Matemática"),
+("Maria", 22, "História"),
+("Pedro", 21, "Ciência da Computação"),
+("Ana", 19, "Biologia"),
+("Carlos", 23, "Economia");
+
+
+CREATE TABLE professor(
+id int auto_increment PRIMARY KEY,
+nome varchar(50) NOT NULL,
+idade int NOT NULL,
+disciplina varchar(50) NOT NULL
+);
+
+INSERT INTO professor(nome, idade, disciplina)
+VALUES
+("Jonas", 37, "Matemática"),
+("Leila", 46, "História"),
+("Martina", 39, "Biologia"),
+("Alexandre",52 ,"Ciência da Computação");
+
+
+CREATE TABLE matricula(
+id int auto_increment PRIMARY KEY,
+id_aluno int, 
+id_professor int,
+data_matricula DATE,
+
+FOREIGN KEY (id_aluno) references aluno(id),
+FOREIGN KEY (id_professor) references professor(id)
+);
+
+INSERT INTO matricula(id_aluno, id_professor, data_matricula)
+VALUES
+(1, 1, '2020-01-15'),
+(2, 2, '2020-02-20'),
+(3, 3, '2020-03-10'),
+(4, 1, '2020-04-05'),
+(5, 2, '2020-05-12');
